bfa445f37ccfe726bb5f9945c51c515ff224d42c
lchavez14 <lic4544@utulsa.edu>
2024-11-08 23:53:52 -0600

    Finished PProblem 6

--- problem 6/fib.py
+++ problem 6/fib.py
@@ -13,4 +13,4 @@ def fibonacci(n):
 
 if __name__ == '__main__':
     start_num = int(input())
-    print(f'fibonacci({start_num}) is {fibonacci(start_num)}')
+    print(f'fibonacci({start_num}) is {fibonacci(start_num)}') 
